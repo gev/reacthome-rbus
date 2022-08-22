07b35c6552042a9b656e4d85b6e5f09aaf8995f8
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-08-22 15:28:10 +0300

    check buffer length

--- src/rbus/Rbus.hs
+++ src/rbus/Rbus.hs
@@ -1,7 +1,7 @@
 module Rbus where
 
 import           Control.Monad           (forever)
-import           Data.ByteString         (ByteString)
+import           Data.ByteString         as B
 import           Data.ByteString.Builder (byteStringHex, toLazyByteString)
 import           Serial                  (recv, withSerial)
 import           Serial.Types
@@ -18,4 +18,6 @@ runRbus :: FilePath -> IO ()
 runRbus device = withSerial device rbusSerialSettings
   $ \port -> forever $ do
     res <- recv port 1
-    print $ device <> ": " <> prettyShow res
+    if B.length res > 0
+      then print $ device <> ": " <> prettyShow res
+      else pure ()
