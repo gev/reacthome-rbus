efa4034b177daeac6f67d3cd3bdf2d8dacb2aed5
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-08-25 00:26:48 +0300

    up

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,14 @@
 module Main where
 
--- import           System.RaspberryPi.GPIO
-import           Control.Concurrent.Async (concurrently_)
-import           Rbus                     (runRbus)
+import           System.RaspberryPi.GPIO
+-- import           Control.Concurrent.Async (concurrently_)
+-- import           Rbus                     (runRbus)
 
 main :: IO ()
--- main = do
---   setPinFunction Pin10 Output
---   setPinFunction Pin19 Output
-main = 
-  concurrently_
-    (runRbus "/dev/ttyAMA1")
-    (runRbus "/dev/ttyAMA2")
+main = do
+  setPinFunction Pin10 Output
+  setPinFunction Pin19 Output
+-- main =
+--   concurrently_
+--     (runRbus "/dev/ttyAMA1")
+--     (runRbus "/dev/ttyAMA2")
