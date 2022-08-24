bd587d68209d5c738910f7480c4083bd2c2d2028
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-08-25 00:45:30 +0300

    try to write gpio

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,17 @@
 module Main where
 
 import           System.RaspberryPi.GPIO
+import           System.RaspberryPi.GPIO (writePin)
 -- import           Control.Concurrent.Async (concurrently_)
 -- import           Rbus                     (runRbus)
 
 main :: IO ()
 main = do
-  -- res <- try $ do 
-    setPinFunction Pin12 Output
-    setPinFunction Pin16 Output
+  writePin Pin12 True
+  writePin Pin16 True
+  -- res <- try $ do
+    -- setPinFunction Pin12 Output
+    -- setPinFunction Pin16 Output
   -- case res of
   --   Left e -> print e
   --   _ -> print "Ok"
