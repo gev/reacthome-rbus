59a84fba3271b3b35db6878de30e3723072d6bb1
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-05-10 00:55:22 +0300

    continue cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -5,35 +5,20 @@ import           Control.Concurrent    (threadDelay)
 import           Control.Monad         (forever)
 import qualified Data.ByteString.Char8 as B
 import           Rbus                  (runRbus)
-import           Serial                (drain, flush, send, withSerial)
 import           System.Posix          (sleep)
 
 
-toggle :: Pin -> IO ()
-toggle gpio = do
-  v <- readPin gpio
-  print $ show gpio <> ": " <> show v
-  writePin gpio $ not v
-
-write :: LogicLevel -> Pin -> IO ()
-write = flip writePin
-
-on :: Pin -> IO ()
-on = write True
-
-off :: Pin -> IO ()
-off = write False
-
 main :: IO ()
-main = withGPIO . withSerial
-      "/dev/ttyAMA1" (SerialPortSettings 9600 8 One NoParity 0)
-      $ \port -> forever $ do
-        on GPIO16
-        send port $ B.pack "Hello world!"
-        drain port
-        -- flush port
-        off GPIO16
-        threadDelay 1_000_000
+main = undefined
+  -- withGPIO . withSerial
+  --     "/dev/ttyAMA1" (SerialPortSettings 9600 8 One NoParity 0)
+  --     $ \port -> forever $ do
+  --       on GPIO16
+  --       send port $ B.pack "Hello world!"
+  --       drain port
+  --       -- flush port
+  --       off GPIO16
+  --       threadDelay 1_000_000
 
 -- main =
 --   concurrently_
