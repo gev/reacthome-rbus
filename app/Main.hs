7c7ed103df6363048a634ca880964f2989d6f458
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-05-10 22:30:19 +0300

    check for empty

--- app/Main.hs
+++ app/Main.hs
@@ -5,5 +5,7 @@ import           Serial
 
 
 main :: IO ()
-main = withSerial "/dev/ttyAMA0" $ \port -> forever $
-        print =<< recv port 512
+main = withSerial "/dev/ttyAMA0" $ \port -> forever $ do
+        bytes <- recv port 512
+        when (bytes /= empty) $
+            print =<< recv port 512
